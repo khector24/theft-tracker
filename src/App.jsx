02a7f5163de77af8e76811b9b012727ae6a3eb4d
Kenny Hector <kennyhector24@gmail.com>
2025-01-08 16:27:41 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import TheftReportForm from './components/TheftReportForm'
+
+function App() {
+  return (
+    <>
+      <TheftReportForm />
+    </>
+  )
+}
+
+export default App
