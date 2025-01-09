543573d5bfe5d2bf5f78d9a717faa488605ebfde
Kenny Hector <kennyhector24@gmail.com>
2025-01-09 16:46:50 -0500

    Handle data change on react form

--- src/components/TheftReportForm.jsx
+++ src/components/TheftReportForm.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import '../styles/component-styles/theft-report-form.css';
 
 export default function TheftReportForm() {
     const [showPeoplePresent, setShowPeoplePresent] = useState(false);
+    const [formData, setFormData] = useState({
+        manager: "",
+        dateTime: "",
+        stolenItems: "",
+        witnesses: "",
+        peoplePresent: "",
+        description: "",
+        files: null
+    });
 
     const handleWitnessChange = (event) => {
         const value = event.target.value;
-        // Show the "People Present" section for "Yes" or "Can't tell"
+
+        setFormData({ ...formData, witnesses: value });
+
         if (value === 'Yes' || value === "Can't tell") {
             setShowPeoplePresent(true);
         } else {
@@ -14,17 +26,56 @@ export default function TheftReportForm() {
         }
     };
 
+    const handleChange = (event) => {
+        setFormData((currData) => {
+            return {
+                ...currData,
+                [event.target.name]: event.target.value,
+            };
+        });
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log("Form was submitted! Good job", formData);
+        setFormData({
+            manager: "",
+            dateTime: "",
+            stolenItems: "",
+            witnesses: "",
+            peoplePresent: "",
+            description: "",
+            files: null
+        });
+    };
+
     return (
         <>
-            <div className="form">
+            <form className="form" onSubmit={onSubmit}>
                 <label htmlFor="">Manager:</label>
-                <input type="text" placeholder="Manager Name" />
+                <input
+                    type="text"
+                    placeholder="Manager Name"
+                    name="manager"
+                    value={formData.manager}
+                    onChange={handleChange}
+                    required />
 
                 <label htmlFor="">Date and time of incident:</label>
-                <input type="datetime-local" />
+                <input
+                    type="datetime-local"
+                    name="dateTime"
+                    value={formData.dateTime}
+                    onChange={handleChange}
+                    required />
 
                 <label htmlFor="">Stolen Items:</label>
-                <input placeholder="What items were stolen?" />
+                <input
+                    placeholder="What items were stolen?"
+                    name='stolenItems'
+                    value={formData.stolenItems}
+                    onChange={handleChange}
+                    required />
 
                 <label htmlFor="">Were there any people present/any possible witnesses?</label>
                 <div>
@@ -34,6 +85,7 @@ export default function TheftReportForm() {
                         name="witnesses"
                         value="Yes"
                         onChange={handleWitnessChange}
+                        required
                     />
                     <label htmlFor="yes">Yes</label>
 
@@ -59,7 +111,11 @@ export default function TheftReportForm() {
                 {showPeoplePresent && (
                     <>
                         <label htmlFor="">People Present:</label>
-                        <input placeholder="Name and contact info if available" />
+                        <input
+                            name="peoplePresent"
+                            value={formData.peoplePresent}
+                            onChange={handleChange}
+                            placeholder="Name and contact info if available" />
                     </>
                 )}
 
@@ -68,10 +124,19 @@ export default function TheftReportForm() {
                     cols="30"
                     rows="10"
                     placeholder="Please describe in detail the events that occurred."
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                    required
                 ></textarea>
 
                 <label>Upload Images/Videos:</label>
-                <input type="file" multiple accept="image/jpeg, video/mp4" />
+                <input
+                    type="file"
+                    multiple
+                    accept="image/jpeg, video/mp4"
+                    onChange={handleChange}
+                    required />
 
                 <div className="bottom-buttons">
                     <button type="reset" style={{ backgroundColor: 'white' }}>
@@ -81,7 +146,7 @@ export default function TheftReportForm() {
                         Submit
                     </button>
                 </div>
-            </div>
+            </form>
         </>
     );
 }
